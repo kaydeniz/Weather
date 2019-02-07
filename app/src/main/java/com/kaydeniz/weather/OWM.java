1656f5aa8f54f76914c511c589f10c81532b5a87
kemalaydeniz <kaydeniz@gmail.com>
2019-02-07 21:14:08 +0300

    Main Activity is created with permissions, ui is designed.

--- app/src/main/java/com/kaydeniz/weather/OWM.java
+++ app/src/main/java/com/kaydeniz/weather/OWM.java
@@ -1,7 +1,10 @@
 package com.kaydeniz.weather;
 
+import android.location.Location;
+
 public class OWM {
 
     public static final String APP_ID="39926ed841202518c1e98da44002be8b";
+    public static Location currentLocation=null;
 
 }
